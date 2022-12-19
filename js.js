3ba61779942aae2cbfa79d886e0a308326291a44
Filip Asplund <filip.asplund@infor.com>
2022-12-19 11:28:18 +0100

    added copy to clipboard

--- js.js
+++ js.js
@@ -10,3 +10,29 @@ window.onload = (event) => {
       document.querySelector(".warning").style.display="block"
    }
 }
+
+function copyToClipboard() {
+
+   var doc = document
+   , text = doc.getElementById("content")
+   , range, selection;
+
+if (doc.body.createTextRange)
+{
+   range = doc.body.createTextRange();
+   range.moveToElementText(text);
+   range.select();
+} 
+
+else if (window.getSelection)
+{
+   selection = window.getSelection();        
+   range = doc.createRange();
+   range.selectNodeContents(text);
+   selection.removeAllRanges();
+   selection.addRange(range);
+}
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+document.getElementById("copy_button").text="Copied";
+}
